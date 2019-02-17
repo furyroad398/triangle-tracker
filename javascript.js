488a96bf9fdddf3ac7084df08f73637d7d4459a0
furyroad398 <mwanziabyn@gmail.com>
2019-02-17 13:54:24 +0300

    created a condition to check if three inputs form a scalene triangle

--- javascript.js
+++ javascript.js
@@ -16,11 +16,11 @@ else if(a + b < c|| a + c < b || b + c < a)
  else if(a == b && a == c && c == b)
 {
     output ="equilateral triangle";
-}/*
+}
 else if (a!=b && a!=c && c!=b)
 {
     output ="scalene triangle";
-}
+}/*
 else if(a == b || a == c || b == c)
 {
  output = "isosceles triangle";
